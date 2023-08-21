0536f6f7a1450c2f6f713493479fd07540d02425
Jen Ziel <jenziel400@gmail.com>
2023-08-21 18:28:34 -0400

    Add file structure

---
+++ src/components/App/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import Ideas from '../Ideas/Ideas';
+import { useState } from 'react';
+function App() {
+    //logic and other needed functions will go here:
+    const dummyIdeas = [{id: 1, title: 'Prank Travis', description: 'Put google eyes on all his stuff'},
+    { id: 2, title: 'Make a secret password app', description: 'So you and your rideshare driver can both know neither one of you is lying' },
+    { id: 3, title: 'Learn a martial art', description: 'To exact vengeance upon my enemies' }]
+    const [ideas, setIdeas] = useState(dummyIdeas)
+
+    return (
+        // your JSX code goes here
+        <main className='App'>
+            <h1>IdeaBox</h1>
+            <p>hi!</p>
+            <Ideas name='Travis' />
+        </main>
+    )
+}
+
+export default App;
---
+++ src/components/Ideas/Ideas.js
@@ -0,0 +1,10 @@
+import './Ideas.css';
+
+function Ideas(props){
+    const {name} = props;
+    return (
+        <h2>Hello, {name}!</h2>
+    )
+}
+
+export default Ideas;
